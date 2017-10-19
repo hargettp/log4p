a1be0795733690cbb472d5028bc8fe334d627cd7
Phil Hargett <phil@haphazardhouse.net>
2017-10-18 19:51:09 -0700

    Fix package file and bump version

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(log4p).
-title("Logging for Prolog").
-version('0.0.1').
-author("Phil Hargett","phil@haphazardhouse.net").
-maintainer("Phil Hargett", "phil@haphazardhouse.net").
-packager("Phil Hargett", "phil@haphazardhouse.net").
-home("https://github.com/hargettp/log4p").
+title('Logging for Prolog').
+version('0.0.2').
+author('Phil Hargett','phil@haphazardhouse.net').
+maintainer('Phil Hargett', 'phil@haphazardhouse.net').
+packager('Phil Hargett', 'phil@haphazardhouse.net').
+home('https://github.com/hargettp/log4p').
 provides(logger).
