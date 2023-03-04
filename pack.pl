fec80ccc267229ff66436c4a946f3e807d685a3d
Phil Hargett <phil@haphazardhouse.net>
2023-03-04 12:47:45 -0800

    Updated README

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(log4p).
 title('Logging for Prolog').
-version('0.0.8').
+version('0.0.9').
 author('Phil Hargett','phil@haphazardhouse.net').
 maintainer('Phil Hargett', 'phil@haphazardhouse.net').
 packager('Phil Hargett', 'phil@haphazardhouse.net').
