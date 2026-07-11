d993d534a39c1477435e73cef9c87c923f941fae
Phil Hargett <phil@haphazardhouse.net>
2026-07-11 08:57:49 -0700

    bump version

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(log4p).
 title('Logging for Prolog').
-version('0.0.14').
+version('0.0.15').
 author('Phil Hargett','phil@haphazardhouse.net').
 maintainer('Phil Hargett', 'phil@haphazardhouse.net').
 packager('Phil Hargett', 'phil@haphazardhouse.net').
 home('https://github.com/hargettp/log4p').
-download('https://github.com/hargettp/log4p/releases/*.zip').
+download('https://github.com/hargettp/log4p.git').
 provides(logger).
